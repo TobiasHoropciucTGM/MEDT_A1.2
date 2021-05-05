ae5deadc362ed9b8035bb22ab973b1426c820acc
Tobias Horopciuc <thoropciuc@student.tgm.ac.at>
2021-05-05 15:31:21 +0200

    umfrage erstellen v1

---
+++ php/UmfrageErstellen.php
@@ -0,0 +1,51 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <title>Umfrage Website</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+  <!-- CSS only -->
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
+</head>
+<body>
+
+<nav class="navbar navbar-default">
+  <div class="container-fluid">
+    <div class="navbar-header">
+      <a class="navbar-brand" href="#">MEDT A1.2</a>
+    </div>
+    <ul class="nav navbar-nav">
+      <li><a href="#">Chat</a></li>
+      <li><a class="active"href="#">Umfrage Erstellen</a></li>
+      <li><a href="#">Umfragen</a></li>
+    </ul>
+  </div>
+</nav>
+    <div class="container-fluid">
+        <div class="text-center">
+            <form action="" method="post">
+                <label><p style="color: blue">Umfrage Titel</p></label><br>
+                <input type="text" name="utitle"><br><br>
+                <label>1. Frage</label><br>
+                <input type="text" name="frage1"><br><br>
+                <label>2. Frage</label><br>
+                <input type="text" name="frage2"><br><br>
+                <label>3. Frage</label><br>
+                <input type="text" name="frage3"><br><br>
+                <input type="submit" value="Umfrage erstellen">
+            </form>
+        </div>
+    </div>
+    <?php 
+    try {
+        $db = new PDO('mysql:host=localhost; dbname=test', "root" , "" );
+        $statement = $db->prepare("Insert INTO");
+    } catch (PDOException $e) {
+        echo "Error!: " . $e->getMessage() . "<br/>";
+        
+    }
+?>
+</body>
+</html>

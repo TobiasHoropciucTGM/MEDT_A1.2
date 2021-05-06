b56c9a227af24744a03cfea78ff50a376be31e11
shulek <shulek@student.tgm.ac.at>
2021-05-06 16:18:46 +0200

    ich check nix

--- php/config.php
+++ php/config.php
@@ -1,8 +1,3 @@
 <?php
-<<<<<<< HEAD
     $pdo = new PDO('mysql:host=localhost;dbname=medta12', 'root', '');
-    
-=======
-    $pdo = new PDO('mysql:host=localhost;dbname=MEDTA12', 'root', '');
->>>>>>> 68ec14359373800c9d4e8f6db89a51fb4cbd3055
 ?>

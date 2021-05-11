8ff0875ffcb9b54b7dfd558841c7caffbe78b334
shulek <shulek@student.tgm.ac.at>
2021-05-11 14:28:05 +0200

    reaload

--- js/reload.js
+++ js/reload.js
@@ -31,42 +31,7 @@ function reload() {
                             messageBody.scrollTop = messageBody.scrollHeight - messageBody.clientHeight;
                         }       
                     }
-                    //console.log(response);
-                    /*if(response != false) {
-                        if(response[3] == response[2]) {
-                            $("#chat-mes").append('<h2 style="text-align: right" id="'+response[1]+'">'+response[0] + ' von: ' + response[2]+'</h2>');
-                        } else {
-                            $("#chat-mes").append('<h2 style="text-align: left" id="'+response[1]+'">'+response[0] + ' von: ' + response[2]+'</h2>');
-                        }
-                        var messageBody = document.querySelector('#chat-mes');
-                        messageBody.scrollTop = messageBody.scrollHeight - messageBody.clientHeight;
-                    }*/
                 }
-                /*  
-                response = jQuery.parseJSON(response);
-                //console.log(response);
-                if(response != false) {
-                    if(response[3] == response[2]) {
-                        $("#chat-mes").append('<h2 style="text-align: right" id="'+response[1]+'">'+response[0] + ' von: ' + response[2]+'</h2>');
-                    } else {
-                        $("#chat-mes").append('<h2 style="text-align: left" id="'+response[1]+'">'+response[0] + ' von: ' + response[2]+'</h2>');
-                    }
-                    var messageBody = document.querySelector('#chat-mes');
-                    messageBody.scrollTop = messageBody.scrollHeight - messageBody.clientHeight;
-                }*/
         });
-    } /*else {
-        $.post('../php/send.php?action=getMessage&latestId=0', function(response){
-            response = jQuery.parseJSON(response);
-            if(response != false) {
-                if(response[3]==response[2]) {
-                    $("#chat-mes").append('<h2 style="text-align: right" id="'+response[1]+'">'+response[0] + ' von: ' + response[2]+'</h2>');
-                } else {
-                    $("#chat-mes").append('<h2 style="text-align: left" id="'+response[1]+'">'+response[0] + ' von: ' + response[2]+'</h2>');
-                }
-                var messageBody = document.querySelector('#chat-mes');
-                messageBody.scrollTop = messageBody.scrollHeight - messageBody.clientHeight;
-            }
-        });
-    }*/
+    }
 }

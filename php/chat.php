ad18978f3c642bfeb76f0b2c3fea201d60cf2313
shulek <shulek@student.tgm.ac.at>
2021-05-11 14:28:27 +0200

    chat

--- php/chat.php
+++ php/chat.php
@@ -4,31 +4,32 @@
         <title>test</title>
         <link rel="stylesheet" type="text/css" href="../css/style.css">
         <!-- Bootstrap -->
-        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+        <!--<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">-->
         <!-- CSS only -->
         <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
-        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
+        <!--<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>-->
+
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-wEmeIV1mKuiNpC+IOBjI7aAzPcEZeedi5yW5f2yOq55WWLwNGmvvx4Um1vskeMj0" crossorigin="anonymous">
         
         
     </head>
     <body>
-        
-        <nav class="navbar navbar-default">
-        <div class="container-fluid">
-            <div class="navbar-header">
-            <a class="navbar-brand" href="#">MEDT A1.2</a>
+        <nav class="navbar navbar-expand-lg navbar-light bg-light">
+            <div class="container">
+                <div id="collapse navbar-collapse">
+                    <ul class="navbar-nav">
+                        <li class="nav-item"><a class="nav-link active" href="#">Chat</a></li>
+                        <li class="nav-item"><a class="nav-link" href="UmfrageErstellen.php">Umfrage erstellen</a></li>
+                        <li class="nav-item"><a class="nav-link" href="eigeneUmfrage.php">Eigene Umfragen</a></li>
+                        <li class="nav-item"><a class="nav-link" href="umfragen.php">Alle Umfragen</a></li>
+                        <li class="nav-item"><a class="nav-link" href="logout.php">LogOut</a></li>
+                    </ul>
+                </div>
             </div>
-            <ul class="nav navbar-nav">
-            <li><a class="active"href="#">Chat</a></li>
-            <li><a href="UmfrageErstellen.php">Umfrage Erstellen</a></li>
-            <li><a href="#">Umfragen</a></li>
-            <li><a href="logout.php">LogOut</a></li>
-            </ul>
-        </div>
         </nav>
 
         <div id="chat" class="container">
-            <div id="chat-mes" class="scrollable-panel">
+            <div id="chat-mes" class="scrollable-panel" style="word-wrap: break-word;  ">
                 <?php
                 
                     include('config.php');
@@ -43,7 +44,7 @@
                         $userf = $user -> fetch();
                         if(strcmp($userf['usersname'], $_SESSION['usersname']) == 0) {
                             echo "<h2 style='text-align:right;margin-bottom:0;' id='".$message['id']."'>".$message['messageText']."</h2>";
-                            echo "<h4 style='text-align:right;margin-top:0;' class='usern'>".$userf['usersname']."</h4>";
+                            echo "<h4 class='usern' style='text-align:right;margin-top:0;' >".$userf['usersname']."</h4>";
                         } else {
                             echo "<h2 style='text-align:left;margin-bottom:0;' id='".$message['id']."'>".$message['messageText']."</h2>";
                             echo "<h4 style='text-align:left;margin-top:0;' class='usern'>".$userf['usersname']."</h4>";
@@ -52,8 +53,8 @@
 
                 ?>
             </div>
-            <div class="col-7">
-                <form style="text-align: center" class="col-5" action="" method="POST">
+            <div class="col-12">
+                <form style="display: block;margin-left: auto;margin-right: auto;" class="col-1" action="" method="POST">
                     <textarea style="text-align: center" class="mesarea" id="messageArea" name="messageArea"></textarea>
                 </form>
             </div>
